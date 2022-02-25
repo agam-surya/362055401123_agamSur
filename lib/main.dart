8d3aef0aadd10a00589ab9eac2204322d6961612
FlutLab.io <>
2022-02-25 09:42:09 +0000

    utsterbaru

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,78 @@
 import 'package:flutter/material.dart';
+import 'signUp.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(debugShowCheckedModeBanner: false, home: FriendlychatApp()));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello world',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({@required this.title});
-
+class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16.0),
-                  color: Colors.green,
+      body: Container(
+        padding: EdgeInsets.fromLTRB(40, 90, 40, 60),
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Column(
+                children: [
+                  Positioned(
+                    child: CircleAvatar(
+                      radius: 55,
+                      backgroundImage: NetworkImage('https://png.pngtree.com/png-clipart/20200310/ourmid/pngtree-indonesia-flag-transparent-watercolor-painted-brush-png-image_2156710.jpg'),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "Welcome to voluntApp",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "mohon dimaklumi karena ketika ditengah tahap pengerjaan laptop saya tiba tiba matot pak, dan sekarang masih diservice",
+                        overflow: TextOverflow.visible,
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 50, 0, 20),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: SizedBox.fromSize(
+                  size: Size.square(400),
+                  child: Image(
+                    image: NetworkImage(
+                      'https://png.pngtree.com/png-clipart/20200310/ourmid/pngtree-indonesia-flag-transparent-watercolor-painted-brush-png-image_2156710.jpg',
+                    ),
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                child: Center(child: Text('njajal cuuuukkkk')),
-              )
-            ],
-          ),
+              ),
+            ),
+            SizedBox(
+              width: 400,
+              height: 50,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.lightBlue),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(13.0),
+                  )),
+                ),
+                onPressed: () {
+                  Route route = MaterialPageRoute(builder: (context) => SignUp());
+                  Navigator.push(context, route);
+                },
+                child: Text(
+                  'SIGN UP',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
